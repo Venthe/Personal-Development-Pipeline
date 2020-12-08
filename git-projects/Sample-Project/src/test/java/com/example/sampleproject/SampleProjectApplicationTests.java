982fd76909d5ef8e4c0ecd7d25d61795511edaa1
Jacek Lipiec <jacek.b.lipiec@gmail.com>
2020-12-08 13:19:00 +0100

    .

---
+++ git-projects/Sample-Project/src/test/java/com/example/sampleproject/SampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
