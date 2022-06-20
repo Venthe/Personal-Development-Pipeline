59775bcb60dbc024dbb2203567114c337cd705b1
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2022-06-20 23:37:13 +0200

    Mark function call in Nexus `manager.sh` as private

--- provisioning/cluster_vagrant/provisioning/2.4_nexus/manager.sh
+++ provisioning/cluster_vagrant/provisioning/2.4_nexus/manager.sh
@@ -5,7 +5,7 @@ NEXUS_USERNAME=admin
 NEXUS_PASSWORD="${NEXUS_PASSWORD:=admin123}"
 NEXUS_URL="${BASE_URL:=localhost:8081}/service/rest"
 
-function call() {
+function _call() {
     local METHOD=$1
     >&2 echo "* Calling with ${method} ${@:2}"
     curl --user "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" \
@@ -18,48 +18,48 @@ function call() {
 }
 
 function GET_documentation() {
-    call GET /swagger.json $@
+    _call GET /swagger.json $@
 }
 
 function PUT_user_password() {
     local USER_ID=$1
     local NEW_PASSWORD=$2
 
-    call PUT "/v1/security/users/${USER_ID}/change-password" \
+    _call PUT "/v1/security/users/${USER_ID}/change-password" \
          --data "${NEW_PASSWORD}" \
          --header 'content-type: text/plain' \
          "${@:3}"
 }
 
 function GET_blobstores() {
-    call GET /v1/blobstores \
+    _call GET /v1/blobstores \
          "${@}"
 }
 
 function POST_blobstores_file() {
     local NAME=$1
-    call POST /v1/blobstores/file \
+    _call POST /v1/blobstores/file \
          --data "{\"name\":\"${NAME}\", \"path\":\"${NAME}\"}" \
          --header 'content-type: application/json' \
          "${@:2}"
 }
 
 function GET_repositories() {
-    call GET /v1/repositories \
+    _call GET /v1/repositories \
          "${@}"
 }
 
 function POST_repositories() {
     local REPOSITORY_TYPE=$1
     local DATA=$2
-    call POST /v1/repositories/${REPOSITORY_TYPE} \
+    _call POST /v1/repositories/${REPOSITORY_TYPE} \
          --data ${DATA} \
          --header "content-type: application/json" \
          "${@:3}"
 }
 
 function GET_anonymous_access() {
-    call GET "/v1/security/anonymous" \
+    _call GET "/v1/security/anonymous" \
          "${@}"
 }
 
@@ -67,7 +67,7 @@ function PUT_anonymous_access() {
     local ENABLED=${1:-true}
     local USER_ID=${2:-anonymous}
     local REALM_NAME=${3:-NexusAuthorizingRealm}
-    call PUT "/v1/security/anonymous" \
+    _call PUT "/v1/security/anonymous" \
          --data "{\"enabled\":${ENABLED},\"userId\":\"${USER_ID}\",\"realmName\":\"${REALM_NAME}\"}" \
          --header 'content-type: application/json' \
          "${@:4}"
@@ -86,14 +86,14 @@ function run_in_docker() {
 }
 
 function PUT_security_realms_active() {
-    call PUT /v1/security/realms/active \
+    _call PUT /v1/security/realms/active \
          --header 'content-type: application/json' \
          --data "${1}" \
          ${@:2}
 }
 
 function wait_for_nexus() {
-    until [[ $(call GET /v1/blobstores --output /dev/null --silent --head --fail; echo $?) == 0 ]]; do
+    until [[ $(_call GET /v1/blobstores --output /dev/null --silent --head --fail; echo $?) == 0 ]]; do
         printf '.'
         sleep 5
     done
