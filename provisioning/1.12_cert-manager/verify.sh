c5b09e4ae1fbd84d4134505980715c02a2be2398
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2022-09-04 22:26:56 +0200

    Add CertManager to ansible provisioning

--- provisioning/1.12_cert-manager/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl run curl -i --tty --image=curlimages/curl --restart=Never --rm -- https://docker.home.arpa -vvv
-kubectl run openssl -i --tty --image=frapsoft/openssl --restart=Never --rm -- verify -verbose -x509_strict
-
-curl https://docker.home.arpa -vvv
-openssl verify -verbose -x509_strict
-openssl s_client -showcerts -servername docker.home.arpa -connect docker.home.arpa:443 2>/dev/null
