58c6aa249d359a5cc8eb4b99aafb9f9a720a6d8a
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2023-02-04 02:04:30 +0100

    Add raw binary loader

---
+++ projects/Experiments/nexus/deploy-raw.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o xtrace
+set -o errexit
+
+. ./.env
+
+function upload() {
+    curl \
+        --fail \
+        --user admin:secret \
+        --upload-file "raw-binaries/${1}" \
+        "https://nexus.home.arpa/repository/raw-hosted/${2}/${1}"
+}
+
+upload "buildkit-v0.11.2.linux-amd64.tar.gz" docker
+upload "docker-20.10.22.tgz" docker
+upload "nerdctl-1.1.0-linux-amd64.tar.gz" docker
+upload "gradle-7.6-all.zip" java/gradle
+upload "kubectl-linux-adm64-v1.26.0" kubernetes
+upload "zulu17.38.21-ca-jdk17.0.5-linux_x64.tar.gz" java/jdk
+upload "helm-v3.11.0-linux-amd64.tar.gz" kubernetes
