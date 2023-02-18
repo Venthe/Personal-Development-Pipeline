5189c4afc3763f4708efcaac455f71b63f077b8a
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2023-02-18 17:12:39 +0100

    Remove obsolete Docker-Environment experiment

--- projects/Experiments/Docker-Environment/Jenkins/Pipeline/jenkins-cli.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --interactive --tty --entrypoint="bash" docker.io/jenkins/jenkins:2.356-jdk11
--- projects/Experiments/Docker-Environment/Jenkins/slave/manager.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DOCKER_BUILDKIT=0 docker build . \
-  --tag docker.io/venthe/jenkins-slave:latest \
-  --tag venthe/jenkins-slave:latest \
-  --tag docker.io/venthe/jenkins-slave:latest-jdk11
