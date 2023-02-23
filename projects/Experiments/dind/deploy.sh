027d76f01949bdae8f76d1b2845ce59c5558106b
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2023-02-23 18:53:47 +0100

    Add docker daemon to Argo deployments

--- projects/Experiments/dind/deploy.sh
+++ projects/Experiments/dind/deploy.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-helm upgrade --install --namespace=infrastructure docker `pwd`/
+set -o errexit
+set -o pipefail
+
+#helm upgrade --install --namespace=infrastructure docker `pwd`/
+
+mkdir -p dist && helm package -d dist ./
+
+helm plugin install https://github.com/chartmuseum/helm-push || true
+helm repo add nexus https://nexus.home.arpa/repository/helm-hosted/ || true
+helm cm-push \
+--username=admin \
+--password=secret \
+./dist/docker-0.1.0.tgz \
+--context-path=/repository/helm-hosted/ \
+nexus
+
+helm repo update
+helm search repo nexus
