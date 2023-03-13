cff623c7f9a9f27c8ce3322a45e6f1a3cbe6c0b7
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2023-03-13 20:21:08 +0100

    Add versioned Setup Maven action

--- projects/Worker-Images/Actions-Runner/actions/setup-maven/src/index.ts
+++ projects/Worker-Images/Actions-Runner/actions/setup-maven/src/index.ts
@@ -1,14 +1,39 @@
-import { callbacks, context, download, RepositoryType, untar } from '@pipeline/core';
-import { shell } from '@pipeline/process';
+import {callbacks, context, download, RepositoryType, step, untar} from '@pipeline/core';
+import {ActionStepDefinition} from '@pipeline/types';
 
-(async function() {
-  const filename = `${context.internal.binariesDirectory}/maven.tar.gz`;
-  await download({
-    sourcePath: 'java/maven/apache-maven-3.9.0-bin.tar.gz',
-    targetPath: filename,
-    context: context,
-    type: RepositoryType.System
-  });
-  await untar(filename, context.internal.binariesDirectory);
-  callbacks.addToPath(`${context.internal.binariesDirectory}/apache-maven-3.9.0/bin`);
+type With = {
+    version?: string,
+};
+
+function toString(str?: string | number) {
+    if (!str) {
+        return ""
+    }
+    return typeof str === "number" ? str.toString() : str;
+}
+
+function mapVersion() {
+    let _with = (step as ActionStepDefinition<With>)?.with ?? {};
+    if (_with.version === undefined) {
+        return "3.9.0"
+    }
+
+    if (toString(_with.version).startsWith("3")) {
+        return "3.3.9"
+    }
+
+    throw new Error("Unsupported version")
+}
+
+(async function () {
+    const version: string = mapVersion()
+    const filename = `${context.internal.binariesDirectory}/maven.tar.gz`;
+    await download({
+        sourcePath: `java/maven/apache-maven-${version}-bin.tar.gz`,
+        targetPath: filename,
+        context: context,
+        type: RepositoryType.System
+    });
+    await untar(filename, context.internal.binariesDirectory);
+    callbacks.addToPath(`${context.internal.binariesDirectory}/apache-maven-${version}/bin`);
 })();
