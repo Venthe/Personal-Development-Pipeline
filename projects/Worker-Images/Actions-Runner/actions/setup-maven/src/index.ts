2fa8ec8d00ff00f66507f0a1e3ba058a45454495
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2023-03-15 20:40:23 +0100

    Add mirrored maven repository to Action-Runner

--- projects/Worker-Images/Actions-Runner/actions/setup-maven/src/index.ts
+++ projects/Worker-Images/Actions-Runner/actions/setup-maven/src/index.ts
@@ -1,5 +1,7 @@
 import {callbacks, context, download, RepositoryType, step, untar} from '@pipeline/core';
 import {ActionStepDefinition} from '@pipeline/types';
+import {shell, shellMany} from "@pipeline/process";
+import * as fs from "fs";
 
 type With = {
     version?: string,
@@ -36,4 +38,37 @@ function mapVersion() {
     });
     await untar(filename, context.internal.binariesDirectory);
     callbacks.addToPath(`${context.internal.binariesDirectory}/apache-maven-${version}/bin`);
-})();
+
+    const settingsXml = `
+<?xml version="1.0" encoding="UTF-8"?>
+<settings xmlns="http://maven.apache.org/SETTINGS/1.1.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.1.0 http://maven.apache.org/xsd/settings-1.1.0.xsd">
+
+  <servers>
+    <server>
+      <id>nexus-snapshots</id>
+      <username>${context.secrets.NEXUS_USERNAME}</username>
+      <password>${context.secrets.NEXUS_PASSWORD}</password>
+    </server>
+    <server>
+      <id>nexus-releases</id>
+      <username>${context.secrets.NEXUS_USERNAME}</username>
+      <password>${context.secrets.NEXUS_PASSWORD}</password>
+    </server>
+  </servers>
+
+  <mirrors>
+    <mirror>
+      <id>central</id>
+      <name>central</name>
+      <url>https://nexus.home.arpa/repository/maven-group/</url>
+      <mirrorOf>*</mirrorOf>
+    </mirror>
+  </mirrors>
+
+</settings>`
+
+    await shell(`mkdir .m2`, {cwd: process.env.HOME})
+    fs.writeFileSync(`${process.env.HOME}/.m2/settings.xml`, settingsXml);
+ })();
