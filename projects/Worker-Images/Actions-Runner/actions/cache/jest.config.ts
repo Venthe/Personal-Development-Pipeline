cdbcdb3420f48c8ce49d4f51d42e7f248d22ac77
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2023-03-19 22:52:03 +0100

    Add scaffold for cache action

---
+++ projects/Worker-Images/Actions-Runner/actions/cache/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'Process library',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[jt]sx?$': ['ts-jest', { tsconfig: `<rootDir>/tsconfig.spec.json` }]
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: 'coverage/libraries/core',
+  setupFilesAfterEnv: ['./test/config.ts']
+};
